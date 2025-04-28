2c4df990d5fe024204fd5882d5cfcd2b4ab9e672
tmichele <tati.micheletti@gmail.com>
2025-04-28 19:53:23 +0000

    Added todo list

---
+++ TODO.R
@@ -0,0 +1,5 @@
+# TODO
+# [ ] Find a hash for an area that actually has forestry for first example! 
+# --> (probably not random and bigger totalArea = 5000000000 + XXX)
+# [ ] Will I run more reps? I think I should!
+# [ ] Test Examples results! --> Functions to do analyis + plots?
